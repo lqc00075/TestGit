4c5d289bba80ace94104fe24e95eaf79063a42fe
lqc00075 <2083873121@qq.com>
2025-07-12 15:33:15 +0800

    写了一个循环

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -7,6 +7,17 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("第二次提交测试");
             Console.WriteLine("第三次提交测试");
+            Console.WriteLine("第四次提交测试");
+            int[] arr = new int[6] { 2, 5, 3, 6, 1, 2 };
+            PrintfArr(arr);
+
+        }
+        public static void PrintfArr(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
         }
     }
 }
